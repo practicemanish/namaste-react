ed1c821cc93a6574e040dd1b11d2a4c01b8c4954
Manish Yadav <manish133188@gmail.com>
2025-08-30 17:59:05 +0530

    state variable in class component

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,12 @@
 import {useState} from 'react';
 const User = ({name}) => {
   const [count] = useState (0);
-  const [count2] = useState(1);
+//   const [count2] = useState(1);
 
   return (
     <div className="user-card">
       <h1>Function Count = {count}</h1>
-       <h1>Function Count = {count2}</h1>
+       {/* <h1>Function Count = {count2}</h1> */}
       <h2>Name:{name}</h2>
       <h3>Location: Dehradoon</h3>
       <h3>Contact me : manish@gmail.com</h3>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,16 +5,28 @@ class UserClass extends React.Component{
 
         this.state ={
             count: 0,
-            count2 :2,
+          
         };
     }
     render(){
         const{name , location} = this.props;
-        const {count,count2} = this.state;
+        const {count} = this.state;
          return (
         <div className="user-card">
             <h1>Class Count:{count}</h1>
-            <h1>Class Count2:{count2}</h1>
+            <button onClick={()=>{
+               //NEVER UPDATE STATE VARIABLE DIRECTLY
+               this.setState({
+                count: this.state.count + 1,
+               });
+            }}>Count Increase</button>
+              <button onClick={()=>{
+               //NEVER UPDATE STATE VARIABLE DIRECTLY
+               this.setState({
+                count: this.state.count - 1,
+               });
+            }}>Count Decrease</button>
+           
             <h2> Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h3>Contact me : manish@gmail.com</h3>
@@ -23,5 +35,5 @@ class UserClass extends React.Component{
         </div>
     );
     }
-};
+}
 export default UserClass;
