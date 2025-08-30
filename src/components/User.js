1fc28a9b1d962db729e448d54e32cc3c401d4b67
Manish Yadav <manish133188@gmail.com>
2025-08-30 17:41:47 +0530

    this is episode 8.3

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,17 @@
-const User = ({name})=> {
-    return (
-        <div className="user-card">
-            <h2>Name:{name}</h2>
-            <h3>Location: Dehradoon</h3>
-            <h3>Contact me : manish@gmail.com</h3>
+import {useState} from 'react';
+const User = ({name}) => {
+  const [count] = useState (0);
+  const [count2] = useState(1);
 
+  return (
+    <div className="user-card">
+      <h1>Function Count = {count}</h1>
+       <h1>Function Count = {count2}</h1>
+      <h2>Name:{name}</h2>
+      <h3>Location: Dehradoon</h3>
+      <h3>Contact me : manish@gmail.com</h3>
 
-        </div>
-    );
+    </div>
+  );
 };
 export default User;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,14 +1,22 @@
 import React from "react";
 class UserClass extends React.Component{
-    constructor (name){
-        super (name);
-        console.log(name);
+    constructor (props){
+        super (props);
+
+        this.state ={
+            count: 0,
+            count2 :2,
+        };
     }
     render(){
+        const{name , location} = this.props;
+        const {count,count2} = this.state;
          return (
         <div className="user-card">
-            <h2> Name: {this.props.name}</h2>
-            <h3>Location: {this.props.location}</h3>
+            <h1>Class Count:{count}</h1>
+            <h1>Class Count2:{count2}</h1>
+            <h2> Name: {name}</h2>
+            <h3>Location: {location}</h3>
             <h3>Contact me : manish@gmail.com</h3>
 
 
