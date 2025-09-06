bd1c4f76269322d40ff7e8aa12d92a349b28d0ee
Manish Yadav <manish133188@gmail.com>
2025-09-06 18:10:40 +0530

    Episode 11.2

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,7 @@ const [searchText, setsearchText] = useState("");
 
 const [filteredRestaurant, setfilteredRestaurant]= useState([]);
 
-// console.log("Body Rendered");
+console.log("Body Rendered",listofRestaurant );
 
 // or we can also write it 
   // const arr = useState(reslist)
