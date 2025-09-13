4544d1860b96286cd7c59e7bae37273f91e92e7e
Manish Yadav <manish133188@gmail.com>
2025-09-13 11:20:25 +0530

    use Of UserContext provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import DarkThemeToggle from "./DarkThemeToggle";
 import Header from "./components/Header";
 // import Body from "./components/Body";
 import { Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
 
 
 
@@ -11,9 +12,25 @@ import { Outlet } from "react-router-dom";
 
 
 const AppLayout = () => {
+
+  const [userName , setUserName] = useState();
+
+  //Authentication 
+  useEffect(()=>{
+    //make an api call and send userName and password
+    const data = {
+      name:"Manish yadav",
+    };
+    setUserName(data?.name);
+  },[]);
+
+
   return (
     <div className="app"> 
+    <UserContext.Provider value={{loggedInUser:userName}}>
       <Header /> 
+    </UserContext.Provider>
+      
       {/* <Body /> */}
       <Outlet />
     </div>
