0ee8472ff7e67d9d58dd48414416e1c96841e029
Manish Yadav <manish133188@gmail.com>
2025-09-14 19:08:25 +0530

    Added cart feature

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,14 @@
 
-
+import { useSelector } from "react-redux";
+import ItemList from "./ItemList"
 const Cart = () =>{
+    const cartItems = useSelector((store) => store.cart.items);
     return(
-        <div>
-            <h1>Hello Manish this is your cart page</h1>
+        <div className="m-4 p-4 text-center">
+            <h1 className="text-2xl font-bold">Hello Manish this is your cart page</h1>
+                <div>
+                    <ItemList items =  {cartItems} />
+                </div>
 
         </div>
     )
