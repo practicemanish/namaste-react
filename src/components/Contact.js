8457ae27a3738ac7890286ea8bd7f99212134581
Manish Yadav <manish133188@gmail.com>
2025-09-19 12:39:58 +0530

    Adding testing libraries

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,12 @@ const Contact = () => {
 
 return(
     <div>
-        <h1>Hello my Contact us page</h1>
+        <h1 className="font-bold text-3xl m-4 p-4">Hello my Contact us page</h1>
+        <form>
+            <input type="text" className="border border-black p-2 m-2" placeholder="Name" />
+            <input type="text" className=" border border-black p-2 m-2" placeholder="message for me " />
+            <button className="border border-gray-700 p-2 m-2 bg-gray-100 rounded-lg">Submit</button>
+        </form>
         
     </div>
 
